924e7c4f9072265d992fb1d586650794b6929a56
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2026-08-31 10:01:01 -0300

    feat: simplify boilerplate and add proper logo
    Signed-off-by: rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,10 @@
-import { serve } from "bun";
-import index from "./index.html";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import "@/index.css";
 
-const server = serve({
-  routes: {
-    // Serve index.html for all unmatched routes.
-    "/*": index,
-
-    "/api/hello": {
-      async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
-
-    "/api/hello/:name": async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
-  },
-
-  development: process.env.NODE_ENV !== "production" && {
-    // Enable browser hot reloading in development
-    hmr: true,
-
-    // Echo console logs from the browser to the server
-    console: true,
-  },
-});
-
-console.log(`🚀 Server running at ${server.url}`);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
